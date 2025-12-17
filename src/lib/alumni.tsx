0cc894a18a8fd379bef0dec61e1f8164e282551e
Tanu Agarwal <162132053+tanu-agarwal0101@users.noreply.github.com>
2025-12-17 09:40:26 +0530

    feat: implement Team page with dynamic member profiles and add TeamMember interface

---
+++ src/lib/alumni.tsx
@@ -0,0 +1,202 @@
+export interface TeamMember {
+  id: string;
+  name: string;
+  role: string;
+  bio: string;
+  image: string;
+  linkedin?: string;
+  github?: string;
+}
+
+export const teamMembers: TeamMember[] = [
+  {
+    id: "1",
+    name: "Piyush Gupta",
+    role: "Web Lead",
+    bio: "Data Structure and Algorithm and a Web Enthusiast. Exploring various technologies in web development. ",
+    linkedin: "https://www.linkedin.com/in/piyush-gupta-536097219",
+    github: "https://github.com/piug-07",
+    image: "/icons/l",
+  },
+  {
+    id: "2",
+    name: "Rudrakshita",
+    role: "Design Lead",
+    bio: "Spearheading user-friendly designs and maintaining visual consistency. My role revolves around crafting engaging experiences for various projects, all within the GDSC ecosystem",
+    linkedin: "https://www.linkedin.com/in/rudrakshita-prasad-4a7041258/",
+    github: "https://www.behance.net/rudyyy2102",
+    image:
+      "https://res.cloudinary.com/dhkxbgwmt/image/upload/v1696706287/Profile%20Photo%20%28File%20responses%29/urnabf4ceig3ldbw5rkm.jpg",
+  },
+  {
+    id: "3",
+    name: "Yash Tiwari",
+    role: "Media Lead",
+    bio: "Developing to Develop !",
+    linkedin: "https://www.linkedin.com/in/yash-tiwari-966648158/",
+    github: "https://github.com/tiwari74",
+    image:
+      "https://res.cloudinary.com/dhkxbgwmt/image/upload/v1696755424/Profile%20Photo%20%28File%20responses%29/yehn5zcn74np2hjh5g1j.jpg",
+  },
+  {
+    id: "4",
+    name: "Shagun Chahar",
+    role: "Android Lead",
+    bio: "Building smarter applications for smarter users.",
+    linkedin: "https://www.linkedin.com/in/shagun-chahar",
+    github: "https://github.com/shags8",
+    image:
+      "https://res.cloudinary.com/dhkxbgwmt/image/upload/v1696706289/Profile%20Photo%20%28File%20responses%29/p3b5jn7f3pqw1xlp9btt.jpg",
+  },
+  {
+    id: "5",
+    name: "Ashish Kushwaha",
+    role: "ML Lead",
+    bio: "ML Lead | Backend Dev | Competitive Programmer",
+    linkedin: "https://www.linkedin.com/in/ashishkingdom/",
+    github: "https://github.com/AshishKingdom",
+    image:
+      "https://res.cloudinary.com/dhkxbgwmt/image/upload/v1696706288/Profile%20Photo%20%28File%20responses%29/ed2fhrc9z0z3u1scfsjz.jpg",
+  },
+  {
+    id: "6",
+    name: "Om Gupta",
+    role: "Programming Lead",
+    bio: "Competitive Programmer | DSA | WEB DEV | Open Source.",
+    linkedin: "https://www.linkedin.com/in/om-gupta-1219b2223/",
+    github: "https://github.com/Omgupta8",
+    image:
+      "https://res.cloudinary.com/diupnvqvv/image/upload/v1668181921/00C86C0C-D1DC-4225-AAAE-99BA74E19CF2_-_21CS048_omgupta_axqrud.jpg",
+  },
+  {
+    id: "7",
+    name: "Rishita Jayant",
+    role: "Web Developer",
+    bio: "Aspiring Full-stack Web Developer ^_^, finding my way through songs is my speciality and Designing's one of my forte...",
+    linkedin: "https://www.linkedin.com/in/rishita-jayant/",
+    github: "https://github.com/Rish-02",
+    image:
+      "https://res.cloudinary.com/dhkxbgwmt/image/upload/v1696755734/Profile%20Photo%20%28File%20responses%29/iyvnjki3rcqfh1aewmeu.jpg",
+  },
+
+  {
+    id: "8",
+    name: "Vrishhti Goel",
+    role: "Designer",
+    bio: "A passionate Designer and DSA enthusiast, striving hard to sharpen my tech skills.",
+    linkedin: "https://www.linkedin.com/in/vrishhti-goel-58aa1b218/",
+    github: "https://github.com/Vrishhti",
+    image:
+      "https://res.cloudinary.com/dhkxbgwmt/image/upload/v1696793000/Profile%20Photo%20%28File%20responses%29/o8qktkfjwqaaomonxgqe.jpg",
+  },
+  {
+    id: "9",
+    name: "Anubhav Pal",
+    role: "Designer",
+    bio: "Developer with a strong idea of UI/UX design, combining technical expertise with a keen eye for aesthetics.",
+    linkedin: "https://www.linkedin.com/in/anubhavpal",
+    github: "https://github.com/Anubhav-Pal",
+    image:
+      "https://res.cloudinary.com/dhkxbgwmt/image/upload/v1696706296/Profile%20Photo%20%28File%20responses%29/bxqtbrghraktx8e0hxwo.jpg",
+  },
+  {
+    id: "10",
+    name: "Devanshi Bahuguna",
+    role: "Programmer",
+    bio: "ML Enthusiast | Data Science | Web Developer",
+    linkedin: "https://www.linkedin.com/in/devanshi-bahuguna-438382174/",
+    github: "https://github.com/devanshibahuguna",
+    image:
+      "https://res.cloudinary.com/diupnvqvv/image/upload/v1668181920/1667999208439_-_devanshi_bahuguna_blf3r0.jpg",
+  },
+  {
+    id: "11",
+    name: "Abhishek Sharma",
+    role: "Programmer",
+    bio: "Competitive programming | Development",
+    linkedin: "https://www.linkedin.com/in/abhishek-sharma-2a3764252",
+    github: "https://github.com/AbhiSharma096",
+    image:
+      "https://res.cloudinary.com/dhkxbgwmt/image/upload/v1696828996/Profile%20Photo%20%28File%20responses%29/cipa42pdrrtqfi8lecwg.jpg",
+  },
+  {
+    id: "12",
+    name: "Vaani Pathariya",
+    role: "Web Developer",
+    bio: "Web Development | Machine Learning | Diving into the world of hackathons and turning creativity into innovation.",
+    linkedin: "https://www.linkedin.com/in/vaani-pathariya/",
+    github: "https://github.com/Vaani-pathariya",
+    image:
+      "https://res.cloudinary.com/dhkxbgwmt/image/upload/v1696706288/Profile%20Photo%20%28File%20responses%29/yzgy1jjgriytgi65bevn.jpg",
+  },
+  {
+    id: "13",
+    name: "Shiven Upadhyay ",
+    role: "Web Developer",
+    bio: "Experienced full stack developer with a passion for problem-solving, and a love for DSA challenges.",
+    linkedin: "https://www.linkedin.com/in/nevish302",
+    github: "https://github.com/Nevish-302",
+    image:
+      "https://res.cloudinary.com/dhkxbgwmt/image/upload/v1696793001/Profile%20Photo%20%28File%20responses%29/dtrv6esyst9usof6qa2a.jpg",
+  },
+  {
+    id: "14",
+    name: "Yatendra Upadhyay",
+    role: "Web Developer",
+    bio: "Full Stack Web developer specialised in backend development.|Passionate about solving real-life problems through Data structures and algorithms. ",
+    linkedin: "https://www.linkedin.com/in/yatendra-upadhyay-33b84a202/",
+    github: "https://github.com/Upadhyay-Yatendra",
+    image:
+      "https://res.cloudinary.com/dhkxbgwmt/image/upload/v1696706317/Profile%20Photo%20%28File%20responses%29/ixuxxqwfsavnaf92xpxf.jpg",
+  },
+  {
+    id: "15",
+    name: "Nitish",
+    role: "Android Developer",
+    bio: "Aspiring for infinity",
+    linkedin: "https://www.linkedin.com/in/nitish-kumar-gupta-a84487188",
+    github: "https://github.com/nitish12rm",
+    image:
+      "https://res.cloudinary.com/dhkxbgwmt/image/upload/v1696778966/Profile%20Photo%20%28File%20responses%29/ql0nyjbru2qgsxzrmawj.jpg",
+  },
+  {
+    id: "16",
+    name: "Smriti Singh",
+    role: "Designer",
+    bio: "I have passion for design. I excel at bridging technology and creativity, with a strong technical foundation in programming and a flair for crafting user-friendly interfaces.",
+    linkedin: "https://www.linkedin.com/in/smriti-singh-91270627b/",
+    github: "https://www.behance.net/smritisingh28",
+    image:
+      "https://res.cloudinary.com/dhkxbgwmt/image/upload/v1696759683/Profile%20Photo%20%28File%20responses%29/kltebtdyugyqdux5pnpf.png",
+  },
+  {
+    id: "17",
+    name: "Deepanshu Pandey",
+    role: "Android Developer",
+    bio: "I'm a programmer skilled in C++, Python, and C, with expertise in Android app development using Flutter and backend development using Django.",
+    linkedin: "https://www.linkedin.com/in/deepanshu-pandey-a16155233/",
+    github: "https://github.com/simplybest12",
+    image:
+      "https://res.cloudinary.com/dhkxbgwmt/image/upload/v1696706306/Profile%20Photo%20%28File%20responses%29/nlz5vjrra4wwtkxcwnku.jpg",
+  },
+  {
+    id: "18",
+    name: "Yuvraj Singh Yadav",
+    role: "Android Developer",
+    bio: "Celebrated for expertise in crafting intuitive Android applications with mastery of dart and Kotlin, excelling as a versatile Python developer.",
+    linkedin: "https://www.linkedin.com/in/yuvraj-singh-yadav-aa9488140/",
+    github: "https://github.com/yyuvraj54",
+    image:
+      "https://res.cloudinary.com/dhkxbgwmt/image/upload/v1696706292/Profile%20Photo%20%28File%20responses%29/e2sla1x3fwpboncsgilk.png",
+  },
+  {
+    id: "19",
+    name: "Bhavya Mittal",
+    role: "Programmer",
+    bio: "Experienced programmer with a passion for learning and growing and have a strong background in Data structures, as well as working on projects.",
+    linkedin: "https://www.linkedin.com/in/mittalbhavya1729",
+    github: "https://github.com/MITTALBHAVYA",
+    image:
+      "https://res.cloudinary.com/dhkxbgwmt/image/upload/v1696706289/Profile%20Photo%20%28File%20responses%29/oqi3awcpfpwkmyt3mogk.jpg",
+  },
+];
